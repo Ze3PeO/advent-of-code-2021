1114458afede27937be5b6fb6fc28978ea7ff278
Philipp Lammers <>
2021-12-01 15:50:46 +0100

    Add directories for current week and spice up solution for part 2

--- puzzles/week1/day1.py
+++ puzzles/week1/day1.py
@@ -9,17 +9,15 @@ def p1(inp):
 def p2(inp):
     result = 0
     for i in range(1, len(inp) - 2):
-        sum1 = inp[i] + inp[i + 1] + inp[i + 2]
-        sum2 = inp[i - 1] + inp[i] + inp[i + 1]
-        if sum1 > sum2:
+        if inp[i + 2] > inp[i - 1]:
             result += 1
     return result
 
 
-with open('../input/day1.txt') as f:
+with open('../input/week1/day1.txt') as f:
     inp = []
     for line in f:
         inp.append(int(line))
 
     print('part 1: ' + str(p1(inp)))
-    print('part 1: ' + str(p2(inp)))
+    print('part 2: ' + str(p2(inp)))
