77defb06102897eb7546ec59dac51e1fbd44568c
Philipp <philipp.lammers1@gmail.com>
2021-12-11 11:16:13 +0100

    Add solution for day 11

---
+++ puzzles/week2/day11.py
@@ -0,0 +1,76 @@
+import numpy as np
+
+FIELD_DIMEN = 10
+
+
+def p1(inp):
+    field = [row[:] for row in inp]
+    result = 0
+
+    for step in range(100):
+        has_flashed = np.zeros((FIELD_DIMEN, FIELD_DIMEN), dtype=bool)
+
+        for y in range(FIELD_DIMEN):
+            for x in range(FIELD_DIMEN):
+                increase(field, x, y, has_flashed)
+
+        result += has_flashed.sum()
+
+    return result
+
+
+def p2(field):
+    field = [row[:] for row in inp]
+    steps = 0
+    synchronized = False
+
+    while not synchronized:
+        has_flashed = np.zeros((FIELD_DIMEN, FIELD_DIMEN), dtype=bool)
+
+        for y in range(FIELD_DIMEN):
+            for x in range(FIELD_DIMEN):
+                increase(field, x, y, has_flashed)
+
+        steps += 1
+
+        if has_flashed.sum() == FIELD_DIMEN * FIELD_DIMEN:
+            synchronized = True
+
+    return steps
+
+
+def increase(field, x, y, has_flashed):
+    if not has_flashed[y][x]:
+        field[y][x] += 1
+
+    if field[y][x] > 9:
+        flash(field, x, y, has_flashed)
+
+
+def flash(field, x, y, has_flashed):
+    if has_flashed[y][x]:
+        return
+
+    has_flashed[y][x] = True
+    field[y][x] = 0
+
+    for j in range(y - 1, y + 2):
+        for i in range(x - 1, x + 2):
+            if is_within_bounds(i, j) and (j != y or i != x):
+                increase(field, i, j, has_flashed)
+
+
+def is_within_bounds(x, y):
+    if 0 <= y < FIELD_DIMEN and 0 <= x < FIELD_DIMEN:
+        return True
+    return False
+
+
+with open('../../input/week2/day11.txt') as file:
+    inp = []
+    for line in file.readlines():
+        row = [int(num) for num in list(line.strip())]
+        inp.append(row)
+
+    print('part 1: ' + str(p1(inp)))
+    print('part 2: ' + str(p2(inp)))
